6293204cf1da2e138ea59caafcccbd1546d360a7
marcin.ostaszewski <marcinostaszewski@gmail.com>
2023-01-30 17:46:29 +0100

    add Random Display styles from props

--- src/components/RandomValuesDisplay/RandomValuesDisplay.js
+++ src/components/RandomValuesDisplay/RandomValuesDisplay.js
@@ -4,16 +4,20 @@ import RandomValuesDisplayStyled from './RandomValuesDisplay.styled'
 const RandomValuesDisplay = props => {
     const display = (props.randomVert >= 0) 
         ? (
-            <div className="flex-column">
-                <div >Random coords:</div>
-                <span className="font-bold">{props.randomVert}, {props.randomHori}</span>
+            <div className="display flex-column mx-auto">
+                <p className="display--text">Random coords:</p>
+                <p>
+                    <strong className="display--values">
+                        {props.randomVert}, {props.randomHori}
+                    </strong>
+                </p>
             </div>
         )
         : ''
     ;
 
     return (
-        <RandomValuesDisplayStyled maxSize={props.maxSize}>
+        <RandomValuesDisplayStyled maxSize={props.maxSize} border={props.randomHori > -1}>
             {display}  
         </RandomValuesDisplayStyled>
     )
--- src/components/RandomValuesDisplay/RandomValuesDisplay.styled.js
+++ src/components/RandomValuesDisplay/RandomValuesDisplay.styled.js
@@ -1,14 +1,21 @@
 import styled from '@emotion/styled'
 
 const RandomValuesDisplayStyled = styled.div`
+    align-items: center;
+    border: ${props => props.border ? '1px solid #ccc' : 0};
     display: flex;
     height: ${props => props.maxSize}px;
-    width: 120px;
-    div {
-        font-size: 12px;
-    }
-    span {
-        font-size: 32px;
+    width: ${props => props.maxSize * 2}px;
+    .display {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        &--text {
+            font-size: ${props => props.maxSize / 6}px;
+        }
+        &--values {
+            font-size: ${props => props.maxSize / 2}px;
+        }
     }
 
 `
