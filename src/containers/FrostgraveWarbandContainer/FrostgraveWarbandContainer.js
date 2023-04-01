e26aa863b8f5a039e9559f281b28fd67712425b6
MarcinOstaszewski <marcinostaszewski@gmail.com>
2023-04-02 00:22:17 +0200

    refactor Frostgrave container;

--- src/containers/FrostgraveWarbandContainer/FrostgraveWarbandContainer.js
+++ src/containers/FrostgraveWarbandContainer/FrostgraveWarbandContainer.js
@@ -1,24 +1,120 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { wizardStats, magicSchools } from '../../helpers/constants';
+import { getRandomSoldierName, saveToLocalStorage } from '../../helpers/helperFunctions';
+import SoldiersContainer from '../../components/SoldiersContainer/SoldiersContainer';
 import StyledFrostgraveWarbandContainer from './FrostgraveWarbandContainer.styles';
 import CasterContainer from '../../components/CasterConteiner/CasterContainer';
-import { wizardStats, apprenticeStats } from '../../helpers/constants';
+import Modal from '../../components/Modal/Modal';
 
 const FrostgraveWarbandContainer = () => {
+	const [wizardName, setWizardName] = useState('');
+	const [apprenticeName, setApprenticeName] = useState('');
+	const [soldiersList, setSoldiersList] = useState([]);
+	// const [castersList, setCastersList] = useState([]);
+	const [warbandCost, setWarbandCost] = useState(0);
+	const [showModal, setShowModal] = useState(false);
+	const [soldierToRemoveData, setSoldierToRemoveData] = useState({});
+
+	const updateWizardName = wizardName => {
+		setWizardName(wizardName);
+		saveToLocalStorage('wizard-name', wizardName);
+	}
+	const updateApprenticeName = apprenticeName => {
+		setApprenticeName(apprenticeName);
+		saveToLocalStorage('apprentice-name', apprenticeName);
+	}
+	const handleCasterNameChange = e => {
+		const value = e.target.value;
+		if (Boolean(e.target.dataset.isWizard)) {
+			updateWizardName(value)
+		} else {
+			updateApprenticeName(value);
+		};
+	}
+	const updateSoldiersList = (newSoldiersList) => {
+		setSoldiersList(newSoldiersList);
+		saveToLocalStorage('soldiers-list', newSoldiersList);
+	}
+	const handleSoldierChange = e => {
+		const target = e.target;
+		const isNameChanged = target.classList.contains('js-name-input');
+		const index = target.parentElement.parentElement.dataset.index;
+		const newSoldiersList = [...soldiersList];
+		if (isNameChanged) {
+			newSoldiersList[index].name = target.value;
+		} else {
+			newSoldiersList[index].type = target.value;
+		}
+		updateSoldiersList(newSoldiersList);
+	}
+	const addSoldier = () => {
+		const newSoldiersList = [...soldiersList];
+		newSoldiersList.push({name: getRandomSoldierName, type: 'Thug'});
+		updateSoldiersList(newSoldiersList);
+	}
+	const handleModalRemove = e => {
+		const index =  e.target.parentElement.dataset.index;
+		console.log(index);
+		const newSoldiersList = [...soldiersList];
+		newSoldiersList.splice(index, 1);
+		updateSoldiersList(newSoldiersList);
+		setShowModal(false);
+	}
+	const handleModalCancel = e => {
+		console.log(e.target);
+		setShowModal(false);
+	}
+	const handleShowModal = e => {
+		const index =  e.target.parentElement.dataset.index;
+		console.log(index);
+		const soldierName = soldiersList[index].name;
+		setSoldierToRemoveData({index: index, name: soldierName});
+		setShowModal(true);
+	}
+
+	useEffect(() => {
+		const lsWizardName = JSON.parse(localStorage.getItem('wizard-name'));
+		const lsApprenticeName = JSON.parse(localStorage.getItem('apprentice-name'));
+		// const lsCastersList = localStorage.getItem('casters-list') || [];
+		const lsSoldiersList = JSON.parse(localStorage.getItem('soldiers-list'));
+		lsWizardName && setWizardName(lsWizardName);
+		lsApprenticeName && setApprenticeName(lsApprenticeName);
+		// lsCastersList && setCastersList(lsCastersList);
+		lsSoldiersList && setSoldiersList(lsSoldiersList);
+	},[]);
 
   return (
     <StyledFrostgraveWarbandContainer>
-        <div className='top-container'>
-            <h1>Frostgrave - Wizard & Warband Sheet</h1>
-            <div className='buttons-container'>
-                <button>Add Soldier</button>
-            </div>
-            <div className='warband-cost'>
-                <span className='font-bold'>Warband Cost: </span><span>[[total cost]] gc</span>
-            </div>
-        </div>
+			<div className='top-container'>
+				<h1>Frostgrave Warband Sheet</h1>
+				<div className='buttons-container'>
+					<button onClick={addSoldier}>Add Soldier</button>
+				</div>
+				<div className='warband-cost'>
+					<span className='font-bold'>Warband Cost: </span><span>{warbandCost} gc</span>
+				</div>
+			</div>
 
-        <CasterContainer wizard={wizardStats}/>
-        <CasterContainer apprentice={apprenticeStats}/>
+			<CasterContainer
+				wizardStats={wizardStats}
+				magicSchools={magicSchools}
+				casterName={wizardName}
+				handleCasterNameChange={handleCasterNameChange}/>
+			<CasterContainer
+				baseStats={wizardStats}
+				casterName={apprenticeName}
+				handleCasterNameChange={handleCasterNameChange}/>
+
+			<SoldiersContainer
+				soldiersList={soldiersList}
+				handleSoldierChange={handleSoldierChange}
+				handleShowModal={handleShowModal}/>
+			
+			{showModal && <Modal 
+				handleRemove={handleModalRemove} 
+				handleCancel={handleModalCancel}
+				soldierData={soldierToRemoveData}
+			/>}
 
     </StyledFrostgraveWarbandContainer>
   )
--- src/containers/FrostgraveWarbandContainer/FrostgraveWarbandContainer.styles.js
+++ src/containers/FrostgraveWarbandContainer/FrostgraveWarbandContainer.styles.js
@@ -20,55 +20,10 @@ const StyledFrostgraveWarbandContainer = styled.main`
             background-color: #79c3f0;
         }
     }
-    .warband-cost {
 
-    }
     .font-bold {
         font-weight: bold;
     }
-    /* .character-container {
-        display: flex;
-        flex-direction: column;
-        flex-wrap: nowrap;
-
-        span {
-            border: 1px solid #ddd;
-            padding: 6px;
-            text-align: center;
-        }
-    }
-    .character-header {
-        display: grid;
-        grid-template-columns: 2fr 6fr 2fr 6fr;
-
-        .highlighted {
-            background-color: #cde;
-        }
-
-        input, select {
-            width: 100%;
-            text-transform: uppercase;
-            font-weight: bold;
-            padding: 0px 12px;
-        }
-
-        select {
-            outline: none;
-            border: 0;
-            height: 100%;
-        }
-    }
-    .character-stats-names {
-        display: grid;
-        grid-template-columns: 2fr 2fr 2fr 2fr 2fr 2fr 2fr 2fr;
-        span {
-            font-weight: bold;
-        }
-    }
-    .character-stats-values {
-        display: grid;
-        grid-template-columns: 2fr 2fr 2fr 2fr 2fr 2fr 2fr 2fr;
-    } */
 `
 
 export default StyledFrostgraveWarbandContainer;
