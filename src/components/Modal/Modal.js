aedd8b4d0538956cf59562e42d9626a231ee4264
MarcinOstaszewski <marcinostaszewski@gmail.com>
2023-04-03 09:20:49 +0200

    add StatTest content to Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,18 +1,64 @@
-import React from 'react'
-import StyledModal from './Modal.styled'
+import React, { useState } from 'react';
+import { createTangetNumberButtons, createModifierButtons } from '../../helpers/helperFunctions';
+import StyledModal from './Modal.styled';
 
-const Modal = ({handleRemove, handleCancel, soldierData}) => {
+const Modal = ({handleRemove, handleCancel, showRemoveContent, showStatTestContent, soldierData}) => {
+	const [chosenTN, setChoseTN] = useState();
+	const [chosenModifier, setChoseModifier] = useState();
+	const targetNumbersButtons = createTangetNumberButtons();
+	const modifierNumberButtons = createModifierButtons();
+
+	const handleButtonClick = e => {
+		console.log(e, e.target);
+		e.target.classList.add('active');
+	}
+
+	const removeSoldierContent = showRemoveContent && (
+		<div className='remove-soldier content-container'>
+			<h3>Are you sure you want to</h3>
+			<div className='soldier-name'>
+				remove soldier: <span>#{soldierData.index} - {soldierData.name}?</span>
+			</div>
+			<div className='buttons'>
+				<button className='danger' onClick={handleRemove}>Remove</button>
+				<button className='info' onClick={handleCancel}>Cancel</button>
+			</div>
+		</div>
+	);
+
+	const statTestContent = showStatTestContent && (
+		<div className='stat-test content-container'>
+			<h3>Test your <span>...</span> skill</h3>
+			<div className='test-settings'>
+				<div className='target-numbers-and-modifiers'>
+					<strong>choose TN (Target Number)</strong>
+					<div className='target-numbers' onClick={handleButtonClick}>
+						{targetNumbersButtons}
+					</div>
+					<strong>select a Modifier</strong>
+					<div className='modifiers' onClick={handleButtonClick}>
+						{modifierNumberButtons}
+					</div>
+				</div>
+
+				{/* <div className='combat-settings-and-result'></div>
+				<div className='combat-oponent-settings'></div> */}
+			</div>
+			<div className='buttons'>
+				<button className='success' onClick={handleRemove}>Test Stat</button>
+				<button className='danger' onClick={handleRemove}>Test & Close</button>
+				<button className='info' onClick={handleCancel}>Cancel</button>
+			</div>
+		</div>
+	)
+	
+	
 	const modal = (
 		<StyledModal>
-				<div className='content'>
-					<div className='remove-soldier'>
-						Are you sure you want to
-					</div>
-					<div className='soldier-name'>remove soldier: <span>#{soldierData.index} - {soldierData.name}?</span></div>
-					<div className='buttons'>
-						<button className='danger' onClick={handleRemove}>Remove</button>
-						<button className='info' onClick={handleCancel}>Cancel</button>
-					</div>
+				<div className='modal-content'>
+					{removeSoldierContent}
+
+					{statTestContent}
 				</div>
     </StyledModal>
 	)
--- src/components/Modal/Modal.styled.js
+++ src/components/Modal/Modal.styled.js
@@ -12,49 +12,127 @@ const StyledModal = styled.div`
 	align-items: center;
 	justify-content: center;
 	
-	.content {
-			width: 50%;
-			height: 50%;
-			display: flex;
-			flex-direction: column;
-			justify-content: space-around;
-			align-items: center;
-			border: 6px solid #d77;
-			border-radius: 12px;
-			background-color: #fdf2f2;
+	.modal-content {
+		padding: 0 2.5%;
+		width: 75%;
+		height: 75%;
+		display: flex;
+		justify-content: center;
+		padding: 12śpx 0;
+		border: 6px solid #d77;
+		border-radius: 12px;
+		background-color: #fdf2f2;
 
-			div {
+		div {
+			font-size: 28px;
+		}
+	}
+
+	.content-container {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+		align-items: center;
+	}
+
+	.remove-soldier {
+		.soldier-name {
+			font-size: 28px;
+			font-weight: bold;
+			color: #e33;
+			span {
 				font-size: 28px;
-				&.soldier-name {
-					font-size: 28px;
-					font-weight: bold;
-					color: #e33;
-					span {
-						font-size: 28px;
+			}
+		}
+	}
 
+	.buttons {
+		display: flex;
+		width: 100%;
+		justify-content: space-around;
+
+		button {
+				padding: 12px 24px;
+				font-size: 24px;
+				border-radius: 12px;
+				border: 0;
+		}
+		.danger {
+			background-color: #d77;
+		}
+		.info {
+			background-color: #6bf;
+		}
+		.success {
+			background-color: #4c9;
+		}
+	}
+
+	.stat-test {
+		height: 100%;
+		width: 100%;
+		display: flex;
+		flex-direction: column;
+		justify-content: space-around;
+
+		.test-settings {
+			display: flex;
+			width: 100%;
+
+			.combat-settings-and-result,
+			.combat-oponent-settings,
+			.target-numbers-and-modifiers {
+				display: flex;
+				flex-grow: 1;
+			}
+
+			.target-numbers-and-modifiers {
+				display: flex;
+				align-items: center;
+				flex-direction: column;
+				justify-content: space-around;
+				strong {
+					font-size: 16px;
+					padding: 12px 0;
+				}
+			}
+
+			.target-numbers, .modifiers {
+				display: flex;
+				flex-wrap: wrap;
+				width: 50%;
+				gap: 6px 12px;
+				button {
+					width: calc(20% - 10px);
+					font-weight: bold;
+					padding: 3px 0;
+					background-color: #6bf;
+					border: 0;
+					border-radius: 6px;
+					&:hover {
+						background-color: #7df;
+					}
+					&.minus {
+						background-color: #e66;
+						&.active {
+							background-color: #b33;
+						}
+					}
+					&.plus {
+						background-color: #4c4;
+						&.active {
+							background-color: #080;
+						}
+					}
+					&.active {
+						background-color: #16a;
+						color: #fff;
 					}
 				}
 			}
-	}
-
-
-	.buttons {
-			display: flex;
-			width: 75%;
-			justify-content: space-around;
-
-			button {
-					padding: 12px 24px;
-					font-size: 24px;
-					border-radius: 12px;
-					border: 0;
-			}
-			.danger {
-					background-color: #d77;
-			}
-			.info {
-					background-color: #6bf;
-			}
+		}
 	}
 `
 
